34f513d855e83142fb8c3f2116da548a45fd2ba2
geo653rge <georgekimanikiringa@gmail.com>
2024-02-07 13:09:37 +0300

    hardhattoken

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config';
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+// const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY ||"";
+
+
+
+
+module.exports = {
+  defaultNetwork:"sepolia",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+
+     
+    }
+  }
 };
 
-export default config;
+
